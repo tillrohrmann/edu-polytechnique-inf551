a47535583d7dd0e8947f8c1f0d97897c3883949f
Till Rohrmann <till@nb.(none)>
2012-12-14 18:28:45 +0100

    clean up skyscraper

--- Skyscraper/skyscraper_v2/build.sh
+++
@@ -1,7 +0,0 @@
-ocamlopt -c dimacs_cnf.ml
-
-ocamlopt -c skyscraper_encoder.ml
-ocamlopt dimacs_cnf.cmx skyscraper_encoder.cmx -o skyscraper_encoder
-
-ocamlopt -c skyscraper_decoder.ml
-ocamlopt dimacs_cnf.cmx skyscraper_decoder.cmx -o skyscraper_decoder
